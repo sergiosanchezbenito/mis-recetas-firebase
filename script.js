8f293c48282c48396e31c2175112996e31f88057
Sergio <sergiosanchezbenito@gmail.com>
2025-12-26 20:58:27 +0100

    Corregidos archivos, limpieza JS/HTML

--- script.js
+++ script.js
@@ -3,25 +3,35 @@ import { db } from "./firebase-config.js";
 import { addDoc, collection } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
+const form = document.querySelector("form");
+
 onAuthStateChanged(auth, (user) => {
   if (!user) {
     window.location.href = "login.html";
   }
 });
 
-const form = document.querySelector("form");
-
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  await addDoc(collection(db, "recetas"), {
-    nombre: nombre.value,
-    ingredientes: ingredientes.value,
-    pasos: pasos.value,
-    usuario: auth.currentUser.email,
-    creada: new Date()
-  });
+  const nombre = document.getElementById("nombre").value;
+  const ingredientes = document.getElementById("ingredientes").value;
+  const pasos = document.getElementById("pasos").value;
 
-  alert("Receta guardada");
-  form.reset();
+  try {
+    await addDoc(collection(db, "recetas"), {
+      nombre: nombre,
+      ingredientes: ingredientes,
+      pasos: pasos,
+      usuario: auth.currentUser.email,
+      creada: new Date()
+    });
+
+    alert("Receta guardada correctamente");
+    form.reset();
+
+  } catch (error) {
+    alert("Error al guardar la receta");
+    console.error(error);
+  }
 });
